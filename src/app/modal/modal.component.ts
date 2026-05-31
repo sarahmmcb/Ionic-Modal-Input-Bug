90e6de582ff2aef8742035cdccb6bb8b6b4ecc67
sarahmmcb <sarah.m.mcb@gmail.com>
2026-05-30 20:53:43 -0400

    update modal input

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,13 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit, computed, Signal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
-  template: `<div> Input received: {{ modalInput() }}`,
+  template: `<div> Input received: {{ displayString() }}`,
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
 
   public modalInput = input('');
+  public displayString: Signal<string> = signal('');
   
   constructor() {}
+
+  public ngOnInit() {
+    this.displayString = computed(() => this.modalInput() || "Something else");
+  }
 
 }
